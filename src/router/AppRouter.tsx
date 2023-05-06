a74fc53eca38286219108ff8b020107bc1b4aa11
andrey <chistyakov000@mail.ru>
2023-05-07 00:33:28 +0400

    changing route

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Layout } from '@/components/Layout';
 import { Booking } from '@/pages/Booking';
 
 export const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/question1" element={<Booking />} />
-        </Route>
+        <Route path="/" element={<Booking />} />
       </Routes>
     </BrowserRouter>
   );
